7c7763fdaccf52f23c3652d600fa093284321347
Erik Evenson <erik.e.evenson@gmail.com>
2015-05-27 17:16:52 -0500

    adds gulp

--- puppet/local_modules/dev_base/manifests/init.pp
+++ puppet/local_modules/dev_base/manifests/init.pp
@@ -60,6 +60,13 @@ class dev_base {
   }
 
   # Install global npm packages.  Update npm last.
+  package { 'gulp':
+    ensure   => '3.8.11',
+    notify   => Package['npm'],
+    provider => 'npm',
+    require  => Class['nodejs'],
+  }
+
   package { 'mocha':
     ensure   => '2.2.5',
     notify   => Package['npm'],
